5dbfd83590ae7b9896e64102eb0ee4131f47905c
lehenric <hnrchl@gmail.com>
2019-09-27 11:57:01 +0200

    change tab to 3 spaces as 1 is not enough after conversion

--- generate-md-contents.sh
+++ generate-md-contents.sh
@@ -3,7 +3,7 @@
 
 list=$(grep '^#' $1)
 # changes last '#' for dash and first ones to spaces to keep indentation of MD, then wraps names with []
-dashes=$(echo "$list" |  sed -E 's/# /- /g;s/#/ /g;s/- /- [/;s/$/]/')
+dashes=$(echo "$list" |  sed -E 's/# /- /g;s/#/   /g;s/- /- [/;s/$/]/')
 # performs lowercasification and prepares linkifiable form of header // TODO might not work with non ASCII characters
 links=$(echo "$list" | tr '[:upper:]' '[:lower:]' | sed 's/# /#/g'  | tr -s '#' | tr ' ' '-' | sed 's/[^-a-zA-Z#]//g;s/$/)/')
 paste -d '(' <(echo "$dashes") <(echo "$links")
